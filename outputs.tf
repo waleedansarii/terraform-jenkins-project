3ac622e1179e02e92a9fb2bc0cd4e148af822b78
waleedansarii <waleedansari012@gmail.com>
2026-09-08 11:07:45 +0530

    Add output for EC2 public IP for Ansible integration

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "ubuntu_ami_id" {
   value       = data.aws_ami.ubuntu.id
   description = "Ubuntu AMI ID"
 }
+
+output "ec2_public_ip" {
+  description = "The public IP address of the provisioned EC2 instance"
+  value       = aws_instance.tf-instance.public_ip
+}
